ae66e285dc13078692db732c62f3862809413893
Alpha <ravalasonn@gmail.com>
2019-07-24 11:36:49 +0300

    Version 2.0 : créer un nouveau type de joueur

--- lib/player.rb
+++ lib/player.rb
@@ -31,3 +31,56 @@ class Player
 		return rand(1..6)
 	end
 end
+
+class HumanPlayer < Player
+	attr_accessor :weapon_level
+
+	def initialize(name_player)
+		@weapon_level = 1
+
+		super(name_player)	
+		@life_points = 100
+	end
+
+	def show_state
+		puts "#{self.name} a #{self.life_points} points de vie et une arme de niveau #{self.weapon_level}"
+	end
+
+	def compute_damage
+		return rand(1..6) * @weapon_level	
+	end
+
+	def search_weapon
+		@new_weapon_level = rand(1..6)
+		puts "Tu as trouvé une arme de niveau #{@new_weapon_level}"
+		if @new_weapon_level > @weapon_level
+			@weapon_level = @new_weapon_level
+			puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends"
+		else
+			puts "Elle n'est pas mieux que ton arme actuelle.."
+		end
+	end
+
+	def search_health_pack
+		@health_pack = rand(1..6)
+		if @health_pack == 1
+			puts "Tu n'as rien trouvé..."
+		elsif @health_pack >= 2 && @health_pack <= 5
+			@life_points = @life_points + 50
+			if @life_points > 100
+				@life_points = 100
+			end
+			puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+		else @health_pack == 6
+			@life_points = @life_points + 80
+			if @life_points > 100
+				@life_points = 100
+			end
+			puts "Waow, tu as trouvé un pack de +80 points de vie !"
+		end
+	end
+end
+
+
+
+
